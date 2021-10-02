3d31395cf92c2c7fb281e998facf60e47b8cd8ba
KaiserLudwigIV <paltmx@gmail.com>
2021-10-02 17:13:28 +0200

    commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+	return (
+		<div>
+			<h1>Maximilian Heppner</h1>
+			<h2>Maximilian Heppner</h2>
+			<h3>Maximilian Heppner</h3>
+			<h4>Maximilian Heppner</h4>
+		</div>
+	);
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
